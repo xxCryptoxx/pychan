4e2cd450bbb4dfcfe1b274c96da5a7c5269ee286
Daryl <usansasa@gmail.com>
2021-12-18 21:49:58 +0200

    pychan.py

--- pychan.py
+++ pychan.py
@@ -5,31 +5,30 @@ from pytube import Playlist
 import pytube
 from pathlib import Path
 
-from pytube.extract import video_id
-
+counter = 0
 playlist_url = input(str('Insert Playlist URL: '))
-def main(k):
-    p = Playlist(playlist_url)
-    print(f'Downloading: {p.title}')
 
-    for video in p.videos:
-        try:
-            title = video.title + '.mp3'
-            print(title + " else hit")
+def main(counter):
+    playlist = Playlist(playlist_url).videos
+    for audio in playlist:
+        title = audio.title + ".mp4"
 
-            if (Path(title).is_file()):
-                print(f'{title} already exists.')
-                pass
-            else:
-                print(video.title + " else hit")
-                video.streams.get_audio_only().download()
-                print(title)
-        except pytube.exceptions.AgeRestrictedError as age:
-            print(age)
+        # check to see if the file exists
+        if Path(title).is_file():
+            print(f'{title} already exists.')
+            counter+= 1
             continue
-threads = []
-for k in range(3):
-    t = threading.Thread(target=main, args=(k,))
-    threads.append(t)
-    print(k)
-    t.start()
+        else:
+            # if the file doesnt exist then try download
+            try:
+                print(f'{title} is downloading.')
+                audio.streams.get_audio_only().download()
+                counter+=1
+
+            # if youtube puts an age limit on the file then skip
+            except pytube.exceptions.AgeRestrictedError as age:
+                print(age)
+                counter+=1
+                continue
+
+main(counter)
